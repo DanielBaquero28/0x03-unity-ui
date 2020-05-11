7d70ed11f5eecdbd408f2391fba31977adda3b16
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-05-11 18:10:14 -0500

    [Feature]: Added movement to the Player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+	public Rigidbody rb;
+	public float speed = 500f;
+
+	void FixedUpdate()
+	{
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 vector = new Vector3(horizontal * Time.deltaTime, 0.0f, vertical * Time.deltaTime);
+
+        rb.AddForce(vector * speed);
+	}
+}
