f744dbe95916f8315e1d70e0906467b1b552a19d
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-05-11 18:42:23 -0500

    [Feature]: Created coin prefab

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject target;
+    public Vector3 offset = new Vector3(-1f, 24.75f, -9);
+
+    void LateUpdate()
+    {
+        transform.position = target.transform.position + offset;
+    }
+}
