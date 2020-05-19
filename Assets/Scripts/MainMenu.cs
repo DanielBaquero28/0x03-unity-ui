68ea5b672d090ff1a880420293c91ac6f9011d4f
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-05-19 11:19:44 -0500

    [Feature]: Quitting the game correctly

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary> Class in charge of controlling the Main Menu </summary>
+public class MainMenu : MonoBehaviour
+{
+    /// <summary> Loads the Maze game scene </summary>
+    public void PlayMaze()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    /// <summary> Exits the game </summary>
+    public void QuitMaze()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -33,6 +33,11 @@ public class PlayerController : MonoBehaviour
             health = 5;
             score = 0;
         }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 	void FixedUpdate()
 	{
@@ -99,6 +104,6 @@ public class PlayerController : MonoBehaviour
     IEnumerator LoadScene(float seconds)
     {
         yield return new WaitForSeconds(seconds);
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
 }
